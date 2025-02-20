6a977ac2b752ebb6bef9ca13fdf71e7dd65f20de
fabiomaffioletti <fabio.maffioletti@gmail.com>
2025-02-20 17:29:54 +0100

    Update experience

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,13 @@ function App() {
             {
               name: "Head of Engineering",
               description: [
-                "Led the engineering organization, managed 4 Senior Engineering Managers, 1 Agile Coach, 40 engineers",
-                "Optimized the org to maximize business impact, align with product strategy and improve developer experience"
+                "Led the engineering organization, managed a multicultural team of 4 Senior EMs, 1 Agile Coach, 32 ICs",
+                "Designed and executed technical strategy and engineering roadmap aligned with business in terms of product quality and performances, security and reliability",
+                "Accomplished a ~70% reduction of global defects and reduced response time to critical support requests by ~95%",
+                "Reduced security issues by ~60% by creating and implementing a structured vulnerabilities management process",
+                "Ensured 100% of users issues were addressed by structuring software ownership according to DDD principles",
+                "Collaborated successfully with multiple departments like Product, Support and HR",
+                "Reached all-time-high in engineering eNPS (8.40 out of 10); retained 100% of talents"
               ],
               from: "August 2024",
               to: "Present"
@@ -27,13 +32,11 @@ function App() {
             {
               name: "Senior Engineering Manager",
               description: [
-                "Led 2 strategic product areas, coached and mentored managers and 4 cross-functional teams",
+                "Spearheaded 2 strategic product areas, coached and mentored managers and 4 cross-functional teams",
                 "Collaborated cross-functionally with multiple PMs, designers and UX researchers",
-                "Improved cycle time by ~60% fostering XP and Lean best practices and streamlining teams’ and org’s processes",
-                "Reduced bugs by 70% by promoting QA shift-left, dogfooding/shadowing and ensuring time for quality work",
-                "Fostered a culture of full self-organization and autonomy based on engineers empowerment",
-                "Partnered with the CTO to inject modern management and customer centric culture",
-                "Achieved the highest eNPS (8.84 out of 10) of the whole tech org; retained 100% of talents"
+                "Empowered engineers through self-organization and introduction of XP and Lean best practices, boosting flow efficiency and cycle time by ~60%",
+                "Reduced bugs by 70% by promoting a customer centric mindset, QA shift-left, dogfooding/shadowing",
+                "Achieved the highest eNPS (8.84 out of 10) of the org, leading to 100% talent retention"
               ],
               from: "October 2023",
               to: "August 2024"
@@ -46,12 +49,11 @@ function App() {
             {
               name: "Engineering Manager / Head of Engineering",
               description: [
-                  "Managed 4 built-from-scratch high-performing cross-functional agile development teams in a hyper-growth remote-first scale-up context, co-designed and scaled by 4x the whole tech organization",
+                  "Managed 4 built-from-scratch high-performing cross-functional agile development teams (~25 engineers) in a hyper-growth remote-first scale-up context; scaled the whole tech organization by 4x",
                   "Established technical strategy, OKRs, KPIs and processes that empowered teams to decrease cycle time by ~30%, optimize external services costs by ~40%, reduce tech debt by ~35%, increase conversion rate by ~20%",
-                  "Coordinated with 5 cross-functional business partners (product, design, strategy, operations, marketing) toward a successful accomplishment of the company strategy",
-                  "Led 2 managers and up to 25 software & data engineers through regular 1:1s, coaching and goal setting",
-                  "Reorganized hiring, performance review and compensation processes using a meritocratic dual-track career ladder, which resulted in the retention of 100% of top talents",
-                  "Realized the most diverse and inclusive team of the organization, protecting psychological safety, promoting collaboration, continuous feedback and continuous improvement"
+                  "Coordinated with 5 cross-functional business partners (product, design, strategy, operations, marketing)",
+                  "Reorganized hiring, performance review and compensation using a meritocratic dual-track career ladder",
+                  "Realized the most diverse and inclusive team of the organization; maintained 100% retention of top performers"
               ],
               from: "March 2021",
               to: "September 2023"
@@ -64,9 +66,9 @@ function App() {
             {
               name: "Head of Engineering / CTO",
               description: [
-                "Managed local and remote development teams with 15 direct and indirect reports working on the two Italian classifieds platforms automobile.it and kijiji.it",
-                "Fostered agile adoption and established a culture of collaboration between tech and business which resulted in 0% employees churn rate, increase website speed by ~70%, reduce technical debt by 25%, evolve technologies/architecture, streamline processes",
-                "Participated at the monthly international eBay Classifieds Group CTOs & Architects round tables"
+                "Managed local and remote development teams of 15 engineers across two Italian classifieds platforms",
+                "Drove agile adoption and established a culture of collaboration between tech and business which resulted in 0% employees churn rate, increase website speed by ~70%, reduce technical debt by 25%, evolve technologies/architecture, streamline processes",
+                "Contributed to global strategy through monthly eBay Classifieds Group CTO and Architects roundtables"
               ],
               from: "February 2020",
               to: "March 2021"
@@ -74,11 +76,10 @@ function App() {
             {
               name: "Software Engineering Manager",
               description: [
-                "Guided the technical discussion during the management team’s weekly meeting",
+                "Guided the technical discussion during the management team’s weekly meeting,",
                 "Defined short & long term technical strategy for the evolution of product and infrastructure, analyzed feasibility and provided estimations based on team’s capacity, ensuring 100% delivery on time and budget",
-                "Recruited, formed and led a new technical team of 10 senior professionals, strengthened team culture and motivated engineers according to company's goals & values",
-                "Promoted and achieved cross-functional collaboration with product, data, marketing, business stakeholders",
-                "Continued with development and software/cloud architecture activities for 20% of the time"
+                "Recruited, formed and led a new technical team of 10 senior professionals, strengthened team culture and motivated engineers according to company's goals & values; 20% hands-on software & architecture",
+                "Promoted and achieved cross-functional collaboration with product, data, marketing, business stakeholders"
               ],
               from: "February 2018",
               to: "February 2020"
@@ -146,6 +147,11 @@ function App() {
         date: "July 2007"
       },
       {
+        name: "Escuela Europea de Coaching",
+        description: "Executive Coaching",
+        date: "2024 - 2025"
+      },
+      {
         name: "Remote-how",
         description: "Academy for Managers",
         date: "March 2022"
@@ -162,7 +168,7 @@ function App() {
       }
     ],
     skills: [
-      "People management, Engineering management, Agile methodologies, Hiring, Coaching, Technical strategy, Conflict resolution, Organizational design, Remote management, Nonviolent communication",
+      "People management, Engineering management, Agile methodologies, Hiring, Coaching, Technical strategy, Conflict resolution, Organizational design, Remote management, Nonviolent communication, OKR, Stakeholder management, Scaling engineering teams",
       "System design, Java, Spring, Python, Redis, MySQL, Elasticsearch, RabbitMQ, Docker, TDD, DDD, CI/CD"
     ],
     links: [
